950adfd692ed058869f1d1f23806797fae30b6b0
Socheat Sok <alex@socheat.net>
2021-07-20 18:18:20 +0700

    feat: extend package scripts and devDependencies

--- generator/index.js
+++ generator/index.js
@@ -1,9 +1,12 @@
 module.exports = api => {
     api.extendPackage({
+        scripts: {
+            'test:bundlesize': 'bundlesize'
+        },
         devDependencies: {
             'bundlesize': '^0.18.1'
         }
-    })
+    });
 
-    api.render('./template')
+    api.render('./template');
 }
